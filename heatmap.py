d5f6b78094173997ba0b544b61fcfc6ee90168f0
Theo Dulka <dulka014@d.umn.edu>
2022-12-28 17:36:57 -0600

    Moved the FigureCanvas impl to HeatmapCanvas and now using imshow instead of pcolormesh

--- heatmap.py
+++ heatmap.py
@@ -8,26 +8,27 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt import NavigationToolbar2QT as NavToolbar
 
+class HeatmapCanvas(FigureCanvas):
+	def __init__(self, figure: Figure, array):
+		super().__init__(figure)
+		self._array = array
+
+		self._axes = self.figure.subplots()
+		self.plot()
+
+	def plot(self):
+		self._axes.imshow(self._array, origin='lower', cmap='cool')
+
 class HeatmapWindow(QDialog):
-	def __init__(self):
+	def __init__(self, array):
 		super().__init__()
 		self._main = QWidget()
-		self.canvas = FigureCanvas(Figure())
+		self.canvas = HeatmapCanvas(Figure(), array)
 
 		layout = QVBoxLayout(self._main)
 		layout.addWidget(NavToolbar(self.canvas, self))
 		layout.addWidget(self.canvas)
 		self.setLayout(layout)
-
-		self._axes = self.canvas.figure.subplots()
-		self.plot()
-
-	def plot(self):
-		x, y = np.meshgrid(np.linspace(-2, 2, 100), np.linspace(-2, 2, 100))
-		c = x * (x**2 + y**2) * np.exp(-(x**2 + y**2))
-
-		c_bounds = self._axes.pcolormesh(x, y, c, cmap="cool")
-		self.canvas.figure.colorbar(c_bounds)
 
 def main():
 	# Check whether there is already a running QApplication
@@ -35,7 +36,16 @@ def main():
 	if not qapp:
 		qapp = QApplication(sys.argv)
 
-	window = HeatmapWindow()
+	length, height = 100, 100
+	array = np.zeros((length, height))
+
+	for (idx, _) in np.ndenumerate(array):
+		y = (2 * (idx[0]+0.5) / array.shape[0] - 1) * 2
+		x = (2 * (idx[1]+0.5) / array.shape[1] - 1) * 2
+
+		array[idx] = x * (x**2 + y**2) * np.exp(-(x**2 + y**2))
+
+	window = HeatmapWindow(array)
 	window.setWindowTitle("Heatmap Test")
 	window.show()
 	window.activateWindow()
