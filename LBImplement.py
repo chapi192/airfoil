88f2ccd3280d25852e6a416e9ab4dce942081980
chapi192 <chapi192@d.umn.edu>
2023-01-04 16:32:44 -0500

    Refactored into class structure

--- LBImplement.py
+++ LBImplement.py
@@ -1,24 +1,5 @@
 import numpy as np
-
-from matplotlib import pyplot
-
-def distance(x1, y1, x2, y2):
-    return ( (x1 - x2)**2 + (y1 - y2)**2 )**0.5
-
-# =============================================================================
-#     the cells are the permutations of the window itself. So the window could 
-#     be larger in either direction, but it will still be divided into 400 by 
-#     100 for the LB algo
-# =============================================================================
-numberOfXCells = 3
-numberOfYCells = 2 
-
-tau = 0.53 #kinematic viscocity/timescale
-
-numberIterations = 2
-
-numberLattice = 9
-
+    
 # =============================================================================
 #     node daigram:   
 #     8 1 2
@@ -28,72 +9,81 @@ numberLattice = 9
 #     should be that    
 # =============================================================================
 
-xVelocities = np.array([0, 0, 1, 1, 1, 0, -1 ,-1 ,-1])
-yVelocities = np.array([0, 1, 1, 0, -1, -1, -1, 0 ,1])
-velocityWeights = np.array([4/9, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36])
+def distance(x1, y1, x2, y2):
+    return ( (x1 - x2)**2 + (y1 - y2)**2 )**0.5
 
-# =============================================================================
-#     initial conditions
-# =============================================================================
+class LatticeBolztman:
+    def __init__(self, windowX, windowY):
+        
+        self.numberOfXCells = windowX
+        self.numberOfYCells = windowY
+        self.tau = 0.53 #viscocity timescale
+        self.numberLattice = 9
 
-#note that "F" is the current state of the model. 3 dimensions: X and Y, velocity
-F = np.ones((numberOfXCells, numberOfYCells, numberLattice)) + np.random.randn(numberOfXCells, numberOfYCells, numberLattice)
+    
+        self.xVelocities = np.array([0, 0, 1, 1, 1, 0, -1 ,-1 ,-1])
+        self.yVelocities = np.array([0, 1, 1, 0, -1, -1, -1, 0 ,1])
+        self.velocityWeights = np.array([4/9, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36])
+        
+        #note that "F" is the current state of the model. 3 dimensions: X and Y, nodal values
+        self.F = np.ones((windowX, windowY, 9)) + np.random.randn(windowX, windowY, 9)
 
-#to move the fluid left we assign a rightward velocity to all particles. This means node 3 will have a nonzero value
-F[:,:, 3] = 2.3
-
-#obstacle here is just a cylander. TODO: make any boundry from selected shape
-cylander = np.full((numberOfXCells, numberOfYCells), False)
-
-for x in range(0, numberOfXCells):
-    for y in range(0, numberOfYCells):
-        if(distance(numberOfXCells/2, numberOfYCells/2, x, y) < 13):
-            cylander[x][y] = True
+        
+    def calculateInitial(self):
+    
+        #to move the fluid right we assign a rightward velocity to all particles. 
+        #This means node 3 will have a nonzero value
+        self.F[:,:, 3] = 2.3
+        
+        #obstacle here is just a obstacle. TODO: make any boundry from selected shape
+        self.obstacle = np.full((self.numberOfXCells, self.numberOfYCells), False)
+        
+        for x in range(0, self.numberOfXCells):
+            for y in range(0, self.numberOfYCells):
+                if(distance(self.numberOfXCells/2, self.numberOfYCells/2, x, y) < 13):
+                    self.obstacle[x][y] = True
+    
+    
+    def calcNext(self):
+        
+    # =============================================================================
+    #     this section is for debugging. Note: if youre using this to debug, save yourself 
+    #     some headache and set the number of cells for x and y to be less than 5 each, and
+    #     less than 5 iterations as well. This dumps the format of F: looping through the x
+    #     cells and the acompanying y cells, where each x,y cell has 9 unique nodes for velocity.    
+    # =============================================================================
+        
+        # print('\n   Iteration:', iteration)
+        # for i in range(0, len(F)):
+        #     for j in range(0, len(F[i])):
+        #         print(f'\n     At {i},{j}:\n', F[i][j])
+    
+        #streaming step. 
+        for i, cx, cy in zip(range(9), self.xVelocities, self.yVelocities):
+            self.F[:, :, i] = np.roll(self.F[:, :, i], cx, axis = 1)
+            self.F[:, :, i] = np.roll(self.F[:, :, i], cy, axis = 0)
             
-# =============================================================================
-#     main loop
-# =============================================================================
-
-for iteration in range(numberIterations):
-    
-# =============================================================================
-#     this section is for debugging. Note: if youre using this to debug, save yourself 
-#     some headache and set the number of cells for x and y to be less than 5 each, and
-#     less than 5 iterations as well. This dumps the format of F: looping through the x
-#     cells and the acompanying y cells, where each x,y cell has 9 unique nodes for velocity.    
-# =============================================================================
-    
-    print('\n   Iteration:', iteration)
-    for i in range(0, len(F)):
-        for j in range(0, len(F[i])):
-            print(f'\n     At {i},{j}:\n', F[i][j])
-
-    #streaming step. 
-    for i, cx, cy in zip(range(numberLattice), xVelocities, yVelocities):
-        F[:, :, i] = np.roll(F[:, :, i], cx, axis = 1)
-        F[:, :, i] = np.roll(F[:, :, i], cy, axis = 0)
+        #finding obstacle collisions, and flipping velocities
+        self.boundry = self.F[self.obstacle, :]
+        self.boundry = self.boundry[:, [0, 5, 6, 7, 8, 1, 2, 3, 4]]
         
-    #finding cylander collisions, and flipping velocities
-    boundry = F[cylander, :]
-    boundry = boundry[:, [0, 5, 6, 7, 8, 1, 2, 3, 4]]
-    
-    #recover density and velocities from F
-    rho = np.sum(F, 2)
-    velX = np.sum(F * xVelocities, 2) / rho
-    velY = np.sum(F * yVelocities, 2) / rho
-    
-    #nothing moves inside the object
-    F[cylander, :] = boundry
-    velX[cylander] = 0
-    velY[cylander] = 0
-    
-    #collision
-    Feq = np.zeros(F.shape)
-    for i, cx, cy, w in zip(range(numberLattice), xVelocities, yVelocities, velocityWeights):
-        Feq[:, :, i] = rho * w * (1 + 3 * (cx*velX + cy*velY) + 9 * (cx*velX + cy*velY)**2 / 2 - 3 * (velX**2 + velY**2)/2)
+        #recover density and velocities from F
+        self.rho = np.sum(self.F, 2)
+        self.velX = np.sum(self.F * self.xVelocities, 2) / self.rho
+        self.velY = np.sum(self.F * self.yVelocities, 2) / self.rho
         
-    F = F + -(1/tau) * (F - Feq)
-    
-# =============================================================================
-#    F is calculated so now it just needs to be displayed     
-# =============================================================================
+        #nothing moves inside the object
+        self.F[self.obstacle, :] = self.boundry
+        self.velX[self.obstacle] = 0
+        self.velY[self.obstacle] = 0
+        
+        #collision
+        self.Feq = np.zeros(self.F.shape)
+        for i, cx, cy, w in zip(range(9), self.xVelocities, self.yVelocities, self.velocityWeights):
+            self.Feq[:, :, i] = self.rho * w * (1 + 3 * (cx*self.velX + cy*self.velY) + 9 * (cx*self.velX + cy*self.velY)**2 / 2 - 3 * (self.velX**2 + self.velY**2)/2)
+            
+        self.F = self.F + -(1/self.tau) * (self.F - self.Feq)
+        
+    # =============================================================================
+    #    F is calculated so now it just needs to be displayed     
+    # =============================================================================
